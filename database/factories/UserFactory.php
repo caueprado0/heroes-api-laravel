cd2b0e5aeea9108124680b3928ed343e997cdd96
Caue Prado <caue.prado0@gmail.com>
2018-11-11 14:59:20 -0200

    Definido o novo namespace de user na factory.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,9 +13,9 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(Heroes\User::class, function (Faker $faker) {
+$factory->define(Heroes\Usuario\Model\Usuario::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'nome' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
