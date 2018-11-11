9448a982c1e5a0836ff9519d009da8699254513d
Caue Prado <caue.prado0@gmail.com>
2018-11-11 13:30:37 -0200

    Adicionada a variavel response.

--- app/Personagens/Service/ListarPersonagens.php
+++ app/Personagens/Service/ListarPersonagens.php
@@ -1,13 +1,25 @@
 <?php
 
-namespace Heroes\Laboratorio\Domain\Service;
+namespace Heroes\Personagens\Service;
 
-use Heroes\Contrato\Domain\Model\GravaContrato;
+use Heroes\RequisicaoHttp\Service\Requisicao;
+use Heroes\Common\MarvelApiRequestTrait;
 
 class ListarPersonagens
 {
+    use MarvelApiRequestTrait;
+
+    const URL = "https://gateway.marvel.com/v1/public/characters";
+
     public function get():array
     {
+        $requisicao = new Requisicao();
+        $response = $requisicao->envia($this->getUrl(self::URL));
+
+        if (substr($response->status, 0, 1) == '2') {
+            return $response->corpo['data'];
+        }
+
         return [];
     }
 }
