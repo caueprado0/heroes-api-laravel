ac1ebd4173e2d35fd16e523b454b6c56bdc85095
Caue Prado <caue.prado0@gmail.com>
2018-11-11 17:55:09 -0200

    API para personagens feita.

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,30 @@
 <?php
 
-use Illuminate\Http\Request;
+Route::prefix('v1')->namespace('Api\\v1')->group(function () {
+    Route::get('/', function () {
+        return response()->json(["url" =>"/", "sucesso" => true, "mensagem" => "O projeto Shun Aceite de Contratos é um software fechado desenvolvido pela equipe de TI da Psychemedics. "]);
+    })->name('v1_index');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    //PERSONAGEM
+    Route::prefix('personagem')->group(function () {
+        Route::get('/', 'Personagem@all')
+        ->name('v1_obter_personagens');
+
+        Route::get('/{personagem}', 'Personagem@find')
+        ->name('v1_obter_personagem');
+    });
+
+    Route::group([
+        'prefix' => 'auth'
+    ], function () {
+        Route::post('login', 'AuthController@login');
+        Route::post('signup', 'AuthController@signup');
+
+        Route::group([
+            'middleware' => 'auth:api'
+        ], function () {
+            Route::get('logout', 'AuthController@logout');
+            Route::get('usuario', 'AuthController@user');
+        });
+    });
 });
