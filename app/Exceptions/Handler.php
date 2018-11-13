d77ce9d29967fb09784bc19087ca7a040fedb68d
Caue Prado <caue.prado0@gmail.com>
2018-11-12 23:00:22 -0200

    Adicionado Handler comum.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,12 @@ namespace Heroes\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Foundation\Http\Exceptions\MaintenanceModeException;
 
 class Handler extends ExceptionHandler
 {
@@ -37,15 +43,91 @@ class Handler extends ExceptionHandler
         parent::report($exception);
     }
 
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
-     */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof NotFoundHttpException) {
+            return response()->json([
+                "url" => $request->fullUrl(),
+                "statusCode" => 404,
+                "sucesso" => false,
+                "mensagem" => env('APP_ENV') == 'local'? $exception->getMessage():"URL não encontrada"
+            ], 404);
+        }
+
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return response()->json([
+                "url" => $request->fullUrl(),
+                "statusCode" => 405,
+                "sucesso" => false,
+                "mensagem" => env('APP_ENV') == 'local'? $exception->getMessage():"Método não permitido."
+            ], 405);
+        }
+
+        if ($exception instanceof ModelNotFoundException) {
+            return response()->json([
+                "url" => $request->fullUrl(),
+                "statusCode" => 404,
+                "sucesso" => false,
+                "mensagem" => env('APP_ENV') == 'local'? $exception->getMessage():"Recurso não encontrado. Tente passar um outro ID."
+            ], 404);
+        }
+
+        if ($exception instanceof QueryException) {
+            return response()->json([
+                "url" => $request->fullUrl(),
+                "statusCode" => 409,
+                "sucesso" => false,
+                "mensagem" => env('APP_ENV') == 'local'? $exception->getMessage():"A query informada estava incorreta."
+            ], 409);
+        }
+
+        if ($exception instanceof MorphRelationChildrenException) {
+            return response()->json([
+                "url" => $request->fullUrl(),
+                "statusCode" => 409,
+                "sucesso" => false,
+                "mensagem" => env('APP_ENV') == 'local'? $exception->getMessage():"Relação feita de forma incorreta."
+            ], 409);
+        }
+
+        if ($exception instanceof MaintenanceModeException) {
+            return response()->json([
+                "url" => $request->fullUrl(),
+                "statusCode" => 503,
+                "sucesso" => false,
+                "mensagem" => "A API está em manutenção, favor tentar novamente mais tarde ou entrar em contato com o TI."
+            ], 503);
+        }
+
+        if ($exception instanceof ValidatorException) {
+            return response()->json([
+                "url" => $request->fullUrl(),
+                "statusCode" => 422,
+                "sucesso" => false,
+                "mensagem" => "Os dados fornecidos são inválidos, por favor verificar a chave data com os detalhes.",
+                "data" => $exception->errors(),
+            ], 422);
+        }
+        if ($exception instanceof \Illuminate\Validation\ValidationException) {
+            return response()->json([
+                "url" => $request->fullUrl(),
+                "statusCode" => 402,
+                "sucesso" => false,
+                "mensagem" => "Os dados fornecidos são inválidos, por favor verificar a chave data com os detalhes.",
+                "data" => $exception->errors()
+            ], 422);
+        }
+
+        if ($exception instanceof AuthenticationException) {
+            return response()->json([
+                "url" => $request->fullUrl(),
+                "statusCode" => 422,
+                "sucesso" => false,
+                "mensagem" => "Você não está autenticado. Para realizar essa operação é necessário estar autenticado.",
+            ], 402);
+        }
+
         return parent::render($request, $exception);
     }
 }
+
